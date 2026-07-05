e4071b13388b7d3f9e7399c808b41f5abfdf6480
Naresh K <nare95143@gmail.com>
2026-07-06 01:19:25 +0530

    feat: add content controller for movie management and establish frontend-backend connectivity documentation

--- content-service/content-service/src/main/java/com/zetflix/contentservice/controller/ContentController.java
+++ content-service/content-service/src/main/java/com/zetflix/contentservice/controller/ContentController.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -54,7 +54,8 @@ public class ContentController {
         @PathVariable String movieId){
             return ResponseEntity.ok(contentService.getMovieById(movieId));
         }
-    
+        
+    @GetMapping("/search")
     public ResponseEntity<List<MovieResponse>> searchMovies(
         @RequestParam String title){
             return ResponseEntity.ok(contentService.searchMovies(title));
